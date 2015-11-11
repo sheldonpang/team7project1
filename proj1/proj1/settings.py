6d8d1beaf5117faa942a62630174ba45e87e3add
sheldonpang <sheldonpang1@gmail.com>
2015-11-11 18:51:35 +0800

    leaflet.js

--- proj1/proj1/settings.py
+++ proj1/proj1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-GEOIP_PATH =os.path.join(BASE_DIR, 'geoip')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -96,3 +96,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'staticfiles'),
+)
