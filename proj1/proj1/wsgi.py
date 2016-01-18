3e3827c6fca3de1ffca58bcfe3510e542d46c6d0
sheldonpang <sheldonpang1@gmail.com>
2016-01-18 19:12:52 +0800

    team7project1 release

--- proj1/proj1/wsgi.py
+++ proj1/proj1/wsgi.py
@@ -13,4 +13,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proj1.settings")
 from django.core.wsgi import get_wsgi_application
 
 from dj_static import Cling
+
 application = Cling(get_wsgi_application())
